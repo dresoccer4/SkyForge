2dac4550c77847d26fc209365e882aebcecc98a9
Daniel Lu <danielrevans4@gmail.com>
2025-02-14 16:21:43 -0500

    Added StreamLit.py file for deployment

---
+++ StreamLit.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("Change Text Input Width")
+
+st.write('A standard text input:')
+st.text_input('Write some text here:')
+
+st.write("Text input in columns:")
+buff, col, buff2 = st.columns([1, 3, 1])
+col.text_input('Smaller text window:')
